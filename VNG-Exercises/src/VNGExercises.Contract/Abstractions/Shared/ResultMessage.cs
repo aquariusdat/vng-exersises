a80fd3f147892610d1be7cc076d9415839e9850c
aquariusdat <tondat.dev@gmail.com>
2024-08-16 22:32:24 +0700

    Remove ResultMessage useless

--- VNG-Exercises/src/VNGExercises.Contract/Abstractions/Shared/ResultMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VNGExercises.Contract.Abstractions.Shared;
-public class ResultMessage<T>
-{
-    public bool IsError { get; set; }
-    public string Message { get; set; }
-    public string MessageDetail { get; set; }
-    public T Data { get; set; }
-}
